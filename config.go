74f2727762120ffd0f7049332fe5be7e484becf0
牛秋晨 <24802117@qq.com>
2019-04-02 18:34:58 +0800

    添加解析文件

---
+++ config.go
@@ -0,0 +1,79 @@
+package Basic
+
+import (
+	"encoding/xml"
+	"github.com/NiuStar/utils"
+)
+
+type Config struct {
+	XMLName xml.Name 	`xml:"config"`
+	LogConfig *logConfig `xml:"logpara"`
+	DBConfig *DBConfig `xml:"db_info"`
+	FileConfig *fileConfig `xml:"file"`
+	ServerConfig *serverConfig `xml:"manage"`
+}
+
+type serverConfig struct {
+	Listen_port string `xml:"listen_port"`
+	TLS bool `xml:"tls"`
+	Cert string `xml:"cert"`
+	Key string `xml:"key"`
+	Websocket bool `xml:"websocket"`
+}
+
+func (s *serverConfig)GetPort() string {
+	return s.Listen_port
+}
+
+type logConfig struct {
+	Log_level string `xml:"log_level"`
+	Log_path string `xml:"log_path"`
+	Log_days int `xml:"log_days"`
+}
+
+type DBConfig struct {
+	DB_server string `xml:"db_server"`
+	DB_port string `xml:"db_port"`
+	DB_name string `xml:"db_name"`
+	DB_user string `xml:"db_user"`
+	DB_password string `xml:"db_password"`
+	DB_charset string `xml:"db_charset"`
+
+}
+
+func (db *DBConfig)GetDBSourceName() string {
+	dataSourceName := db.DB_user + ":" + db.DB_password +
+		"@tcp(" + db.DB_server + ":" + db.DB_port + ")/" + db.DB_name
+	if len(db.DB_charset) > 0 {
+		dataSourceName += "?charset=" + db.DB_charset
+	}
+	return dataSourceName
+}
+
+type fileConfig struct {
+	LocalPath string
+	NetworkPath string
+	WebWorkDir string
+}
+
+var (
+	gConfig *Config
+)
+//初始化;
+func init() {
+	gConfig = initConfig()
+}
+
+func initConfig() *Config {
+	data := utils.ReadFileFullPath("./config.xml")
+	var config Config
+	err := xml.Unmarshal([]byte(data),&config)
+	if err != nil {
+		panic(err)
+	}
+	return &config
+}
+
+func GetServerConfig() *Config {
+	return gConfig
+}
